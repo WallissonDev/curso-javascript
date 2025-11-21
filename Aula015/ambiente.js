d9610b44e3f4aa00b3f08757474b7f9f8bc12bba
Wallisson M. <wallissonjob@hotmail.com>
2025-11-21 10:26:09 -0300

    aula 15 - array

---
+++ Aula015/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 2, 9, 3]
+
+for (let pos = 0; pos < num.length; pos++) {
+    console.log(`Item ${num[pos]} na posição ${pos}`)
+}
+
+for (let pos in num ) {
+    console.log(`Item ${num[pos]} na posição ${pos}`)
+} 
